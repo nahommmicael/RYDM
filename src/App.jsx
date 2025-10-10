706bf039a0e43c2fe6f85b4e06131ba878b39149
Nahom Micael <nm078@hdm-stuttgart.de>
2025-10-10 11:04:54 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import "./index.css";
 export default function App() {
   return (
     <div className="app bg-black text-white overflow-hidden">
-      <div
-        className="relative rounded-[40px] overflow-hidden bg-black text-white shadow-2xl"
-        style={{
-          width: "390px",                // echte Breite iPhone 12
-  height: "844px",               // echte Höhe iPhone 12
-  "--scale": "min(calc(100vw / 390), calc(100lvh / 844))",
-  transform: "scale(var(--scale))",
-  transformOrigin: "center center",
-        }}
-        
-      >
-        <TrackProvider>
-          <Home />
-          <PlayerBar />
-        </TrackProvider>
-      </div>
-    </div>
+  <div
+    className="relative rounded-[40px] overflow-hidden bg-black text-white shadow-2xl"
+    style={{
+      width: "390px", // iPhone 12 design reference
+      height: "844px",
+      "--scale": "min(calc(100vw / 390), calc(100lvh / 844))",
+      transform: "scale(var(--scale))",
+      transformOrigin: "top center", // fix center drift
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      translate: "-50% -50%", // perfect centering
+    }}
+  >
+    <TrackProvider>
+      <Home />
+      <PlayerBar />
+    </TrackProvider>
+  </div>
+</div>
   );
 }
